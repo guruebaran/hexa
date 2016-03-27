4dd9484dac5ef17573d58d0b29e55cf74d84cccd
guruebaran <guruebaran@gmail.com>
2016-03-27 21:06:34 +0530

    daily commit

--- newjunk.py
+++ newjunk.py
@@ -15,9 +15,9 @@ def checkSum(dataString,write):
         chkString = '0'
         return (0,chkString)
 
-a = checkSum(dataSting,0)
-if( a == 1):
-    print('yo',a[1])
+#a = checkSum(dataSting,0)
+#if( a == 1):
+ #   print('yo',a[1])
 
 channel = dataSting[0:2]
 command = dataSting[2:4]
@@ -44,6 +44,33 @@ def dataCompiler(channel,command,param11,param12,param21,param22,dataSize11,data
         return 0
 
 
-final = dataCompiler(channel,command,param11,param12,param21,param22,dataSize11,dataSize12,dataSize21,dataSize22,errCode)
-print('wow',final)
+#final = dataCompiler(channel,command,param11,param12,param21,param22,dataSize11,dataSize12,dataSize21,dataSize22,errCode)
+#print('wow',final)
+def fpsTransmitter(data):
+    print('data:',data)
+    print('data :',binascii.unhexlify(data))
+def registrationDataGenerator():
+    #CMD_FP_REGISTER_START(0x51)_
+    channel = '00'
+    command = ['50','51','19']
+    param11= ['ff','00']
+    param12= ['ff','00']
+    param21= '00'
+    param22 = '00'
+    dataSize11= ['05','00']
+    dataSize12= '00'
+    dataSize21= '00'
+    dataSize22= '00'
+    errCode = '00'
+    data = dataCompiler(channel,command[0],param11[0],param12[0],param21,param22,dataSize11[0],dataSize12,dataSize21,dataSize22,errCode)
+    fpsTransmitter(data)
+    data = dataCompiler(channel,command[1],param11[1],param12[1],param21,param22,dataSize11[1],dataSize12,dataSize21,dataSize22,errCode)
+    fpsTransmitter(data)
+    data = dataCompiler(channel,command[2],param11[1],param12[1],param21,param22,dataSize11[1],dataSize12,dataSize21,dataSize22,errCode)
+    fpsTransmitter(data)
+    data = dataCompiler(channel,command[0],param11[0],param12[0],param21,param22,dataSize11[0],dataSize12,dataSize21,dataSize22,errCode)
+    fpsTransmitter(data)
+    data = dataCompiler(channel,command[1],param11[1],param12[1],param21,param22,dataSize11[1],dataSize12,dataSize21,dataSize22,errCode)
+    fpsTransmitter(data)
+
 
